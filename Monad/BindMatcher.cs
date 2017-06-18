88a77d052504aa95b918b411302f3ac037ebb3ec
Jan Felix Reuter <felix@thefogger.de>
2017-06-18 19:06:43 +0200

    Add bind operation

---
+++ Monad/BindMatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Monad
+{
+    internal class BindMatcher<A, Error, B> : IResultMatcher<A, Error, Result<B, Error>>
+    {
+        private readonly Func<A, Result<B, Error>> _selector;
+
+        public BindMatcher(Func<A, Result<B, Error>> selector)
+        {
+            _selector = selector;
+        }
+
+        public Result<B, Error> Success(A value)
+        {
+            return _selector(value);
+        }
+
+        Result<B, Error> IResultMatcher<A, Error, Result<B, Error>>.Error(Error error)
+        {
+            return new Error<B, Error>(error);
+        }
+    }
+}
---
+++ Tests/BindTests.cs
@@ -0,0 +1,36 @@
+﻿using Monad;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class BindTests
+    {
+        [Fact]
+        public void When_Given_A_Success_It_Returns_Selector_Result()
+        {
+            var expectedResult = new Success<int, object>(1337);
+            var success = new Success<int, object>(23);
+
+            var actualResult = success.Bind(i => expectedResult);
+
+            Assert.Same(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void When_Given_An_Error_It_Does_Not_Call_The_Selector()
+        {
+            var error = new Error<int, object>(new object());
+
+            Func<int, Result<int, object>> selector = i =>
+            {
+                Assert.True(false);
+                return null;
+            };
+            
+            var result = error.Bind(selector);
+
+            Assert.IsType<Error<int, object>>(error);
+        }
+    }
+}
